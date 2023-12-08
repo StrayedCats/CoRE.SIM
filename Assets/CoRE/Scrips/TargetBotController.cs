bc515a69805c2a51887c5c5fdda8f1ac1101ee4c
Taiga Takano <ttttghghnb554z@outlook.jp>
2023-12-09 02:02:26 +0900

    v1.3a1

--- Assets/CoRE/Scrips/TargetBotController.cs
+++ Assets/CoRE/Scrips/TargetBotController.cs
@@ -5,18 +5,31 @@ using UnityEngine.AI;
 
 public class TargetBotController : MonoBehaviour
 {
+    public SettingMenuAgent agent;
     private NavMeshAgent bot_nav;
     private Vector3 target_pos;
-    // Start is called before the first frame update
+
+    private Vector3 initPos;
+    
+    public void resetPos()
+    {
+        target_pos = initPos;
+        bot_nav.SetDestination(target_pos);
+        this.transform.position = new Vector3(6.13700008f, 0, -1.28299999f);
+        this.transform.rotation = new Quaternion(0, 0, 0, 1);
+    }
+
     void Start()
     {
         bot_nav = this.GetComponent<NavMeshAgent>();
         target_pos = this.transform.position;
+        initPos = this.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (agent.isShow) { return; }
+
         bot_nav.SetDestination(target_pos);
         if (Input.GetMouseButtonDown(0))
         {
